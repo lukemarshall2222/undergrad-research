8988192c03b100cbc4063c4b1e9e51f29f127c24
Luke Marshall <lukemarshall2222@gmail.com>
2025-04-21 22:27:18 -0700

    finsih off rust

--- rust-files/translation/src/main.rs
+++ rust-files/translation/src/main.rs
@@ -1,12 +1,12 @@
 #![allow(dead_code)]
 
+use std::{cell::RefCell, collections::BTreeMap, io::stdout, rc::Rc};
+
 use builtins::{
-    FilterFunc, GroupingFunc, ReductionFunc, counter, create_distinct_operator,
-    create_epoch_operator, create_filter_operator, create_groupby_operator, create_join_operator,
-    create_map_operator, filter_groups, get_mapped_int, key_geq_int, rename_filtered_keys,
-    single_group, sum_ints,
+    counter, create_distinct_operator, create_epoch_operator, create_filter_operator, create_groupby_operator, create_join_operator, create_map_operator, dump_as_csv, filter_groups, get_mapped_int, key_geq_int, rename_filtered_keys, single_group, sum_ints, FilterFunc, GroupingFunc, ReductionFunc
 };
-use utils::{Headers, OpResult, Operator, OperatorRef};
+use ordered_float::OrderedFloat;
+use utils::{Headers, OpResult, OperatorRef};
 
 mod builtins;
 mod utils;
@@ -532,7 +532,7 @@ fn slowloris(next_op: OperatorRef) -> [OperatorRef; 2] {
                 filter_groups(incl_keys.clone(), &mut headers)
             });
             let reduce_func: ReductionFunc =
-                Box::new(move |init_val: OpResult, mut headers: &mut Headers| {
+                Box::new(move |init_val: OpResult, headers: &mut Headers| {
                     sum_ints("ipv4.len".to_string(), init_val, headers).unwrap()
                 });
             create_epoch_operator(
@@ -593,16 +593,113 @@ fn slowloris(next_op: OperatorRef) -> [OperatorRef; 2] {
                 create_map_operator(mapping_func, create_filter_operator(filter_func, next_op)),
             )
         });
-        let (join_op1, join_op2) = create_join_ops(next_op);
+    let (join_op1, join_op2) = create_join_ops(next_op);
 
-        [n_conns(join_op1), n_bytes(join_op2)]
+    [n_conns(join_op1), n_bytes(join_op2)]
 }
 
-fn create_join_operator_test(next_op: OperatorRef) -> OperatorRef {
+fn create_join_operator_test(next_op: OperatorRef) -> [OperatorRef; 2] {
     let epoch_dur: f64 = 1.0;
-    let syns
+    let mut syns: Box<dyn FnMut(OperatorRef) -> OperatorRef + 'static> =
+        Box::new(move |next_op: OperatorRef| {
+            let filter_func: FilterFunc = Box::new(move |headers: &Headers| {
+                get_mapped_int("ipv4.proto".to_string(), &headers) == 6
+                    && get_mapped_int("l4.flags".to_string(), &headers) == 2
+            });
+            create_epoch_operator(
+                epoch_dur,
+                "eid".to_string(),
+                create_filter_operator(filter_func, next_op),
+            )
+        });
+
+    let mut synacks: Box<dyn FnMut(OperatorRef) -> OperatorRef + 'static> =
+        Box::new(move |next_op: OperatorRef| {
+            let filter_func: FilterFunc = Box::new(move |headers: &Headers| {
+                get_mapped_int("ipv4.proto".to_string(), &headers) == 6
+                    && get_mapped_int("l4.flags".to_string(), &headers) == 18
+            });
+            create_epoch_operator(
+                epoch_dur,
+                "eid".to_string(),
+                create_filter_operator(filter_func, next_op),
+            )
+        });
+
+    let mut join_ops: Box<dyn FnMut(OperatorRef) -> (OperatorRef, OperatorRef) + 'static> =
+        Box::new(move |next_op: OperatorRef| {
+            let left_extractor_func: Box<dyn FnMut(Headers) -> (Headers, Headers) + 'static> =
+                Box::new(move |mut headers: Headers| {
+                    (
+                        rename_filtered_keys(
+                            Vec::from([("ipv4.src".to_string(), "host".to_string())]),
+                            &mut headers,
+                        ),
+                        rename_filtered_keys(
+                            Vec::from([("ipv4.dst".to_string(), "remote".to_string())]),
+                            &mut headers,
+                        ),
+                    )
+                });
+            let right_extractor_func: Box<dyn FnMut(Headers) -> (Headers, Headers) + 'static> =
+                Box::new(move |mut headers: Headers| {
+                    (
+                        rename_filtered_keys(
+                            Vec::from([("ipv4.src".to_string(), "host".to_string())]),
+                            &mut headers,
+                        ),
+                        filter_groups(Vec::from(["time".to_string()]), &mut headers),
+                    )
+                });
+            create_join_operator(None, left_extractor_func, right_extractor_func, next_op)
+        });
+    let (join_op1, join_op2) = join_ops(next_op);
+
+    [syns(join_op1), synacks(join_op2)]
+}
+
+fn q3(next_op: OperatorRef) -> OperatorRef {
+    let incl_keys: Vec<String> = Vec::from(["ipv4.src".to_string(), "ipv4.dst".to_string()]);
+    let groupby_func: GroupingFunc =
+        Box::new(move |mut headers: Headers| filter_groups(incl_keys.clone(), &mut headers));
+    create_epoch_operator(
+        100.0,
+        "eid".to_string(),
+        create_distinct_operator(groupby_func, next_op),
+    )
+}
+
+fn q4(next_op: OperatorRef) -> OperatorRef {
+    let incl_keys: Vec<String> = Vec::from(["ipv4.src".to_string()]);
+    let groupby_func: GroupingFunc =
+        Box::new(move |mut headers: Headers| filter_groups(incl_keys.clone(), &mut headers));
+    create_epoch_operator(
+        10000.0,
+        "eid".to_string(),
+        create_groupby_operator(groupby_func, Box::new(counter), "pkts".to_string(), next_op),
+    )
+}
+
+fn create_query() -> OperatorRef { 
+    ident(Rc::new(RefCell::new(dump_as_csv(None, Some(false), Box::new(stdout())))))
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut _query: OperatorRef = create_query();
+    for i in 0..20 {
+        let mut header: BTreeMap<String, OpResult> = BTreeMap::new();
+        header.insert("time".to_string(), OpResult::Float(OrderedFloat(i as f64)));
+        header.insert("eth.src".to_string(), OpResult::MAC([0x00, 0x11, 0x22, 0x33, 0x44, 0x55]));
+        header.insert("eth.dst".to_string(), OpResult::MAC([0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF]));
+        header.insert("eth.ethertype".to_string(), OpResult::Int(0x0800));
+        header.insert("ipv4.hlen".to_string(), OpResult::Int(20));
+        header.insert("ipv4.proto".to_string(), OpResult::Int(6));
+        header.insert("ipv4.len".to_string(), OpResult::Int(60));
+        header.insert("ipv4.src".to_string(), OpResult::IPv4("127.0.0.1".parse().unwrap()));
+        header.insert("ipv4.dst".to_string(), OpResult::IPv4("127.0.0.1".parse().unwrap()));
+        header.insert("l4.sport".to_string(), OpResult::Int(440));
+        header.insert("l4.dport".to_string(), OpResult::Int(50000));
+        header.insert("l4.flags".to_string(), OpResult::Int(10));
+        (_query.borrow_mut().next)(&mut header)
+    }
 }
